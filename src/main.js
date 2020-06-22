dc5e606516d94544d8962be6ae59e2a576194ed9
Tristan Emmerson <65921828+tmemmerson@users.noreply.github.com>
2020-06-22 16:15:31 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-import { pingPong } from './ping-pong';
+import $ from 'jquery';
 import './styles.css';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 $(document).ready(function() {
   $('#ping-pong-form').submit(function(event) {
